baf08528832639c0b37491d71f22ed9f9cb052aa
Ayushkumar Maurya <ayushskmaurya@gmail.com>
2024-08-03 12:12:42 +0530

    Created tables for Sources & Transactions.
    React Native | Money Tracker | Database

--- React-Native/Money-Tracker/Database/tables.sql
+++ React-Native/Money-Tracker/Database/tables.sql
@@ -7,3 +7,21 @@ CREATE TABLE users (
 	mobile_no VARCHAR(20) UNIQUE NOT NULL,
 	CONSTRAINT users_pk PRIMARY KEY (id)
 );
+
+-- Creating Table sources.
+CREATE TABLE sources (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(15) UNIQUE NOT NULL,
+	CONSTRAINT sources_pk PRIMARY KEY (id)
+);
+
+-- Creating Table transactions.
+CREATE TABLE transactions (
+	id INT AUTO_INCREMENT,
+	source_id INT NOT NULL,
+	description VARCHAR(100),
+	amount DOUBLE NOT NULL,
+	datetime DATETIME DEFAULT NOW() NOT NULL,
+	CONSTRAINT transactions_pk PRIMARY KEY (id),
+	CONSTRAINT transactions_fk FOREIGN KEY (source_id) REFERENCES sources(id)
+);
